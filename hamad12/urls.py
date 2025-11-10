ac570a8233b4426d18b72779bda9ca0f5fceb025
althobaitihamad <althobaitihamad@gmail.com>
2025-11-10 10:36:03 +0300

    22 u3

--- hamad12/urls.py
+++ hamad12/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
+# ===========================
+# روابط المشروع الأساسية
+# ===========================
 urlpatterns = [
-    # لوحة التحكم
+    # لوحة التحكم (Admin Panel)
     path('admin/', admin.site.urls),
 
     # ===========================
@@ -12,3 +17,10 @@ urlpatterns = [
     path('store/', include('store.urls')),   # تطبيق المتجر
     path('orders/', include('orders.urls')), # تطبيق الطلبات
 ]
+
+# ===========================
+# عرض الملفات الثابتة والإعلامية أثناء التطوير
+# ===========================
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
